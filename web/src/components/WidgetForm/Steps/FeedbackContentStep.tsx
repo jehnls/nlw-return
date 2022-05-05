78989779c117e990c32915969af51cd9497cd6d9
Bizonho <bizonho@pop-os.localdomain>
2022-05-05 00:29:52 -0300

    Step(2) : Parei o video no tempo 59:04

---
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,15 @@
+import { CloseButton } from "../../CloseButton";
+
+export function FeedbackContentStep() {
+  return (
+    // ISTOPHERE Time : https://nextlevelweek.com/episodios/impulse/aula-2/edicao/8?utm_source=convertkit&utm_medium=leads_cadastrados&utm_campaign=event-NLW08&utm_term=email&utm_content=stage2-impulse
+    //time 59:04
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
+}
---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Image de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Image de um lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Image de um balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes; //TODO P/ pegar somente a chave de um objeto, para criar tipagem
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  // TOOO Exemplo de reposividade  w-[calc(100vw-2rem)] md:w-auto
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep />
+      )}
+      <footer>
+        Feito com ♥ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://www.rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+// TODO Object.entries(feedbackTypes) => é Melhor para percorrer-lo do que um objeto
+/**
+ * [
+ *  [BUG, {...}],
+ *  [IDEA, {...}],
+ *  [OTHER, {...}]
+ * ]
+ *
+ */
